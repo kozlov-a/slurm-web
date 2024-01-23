b425e67391b6e23c9fd5c8b10c843c81ae179da7
kav <kozlov-a@speechpro.com>
2024-01-23 18:03:33 +0300

    add run with ldap

---
+++ docker/run/run_sssd.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# EDIT: indicate where your configuration files will be stored
+data=$PWD
+
+docker run -d -v $data/conf:/etc/slurm-web \
+              -v /etc/munge:/etc/munge \
+              -v /etc/slurm:/usr/local/etc/ \
+              -v /etc/passwd:/etc/passwd \
+              -v /etc/group:/etc/group \
+              -v /etc/sssd/sssd.conf:/etc/sssd/sssd.conf \
+              -p 8081:80 \
+              slurm-web-sssd
+
